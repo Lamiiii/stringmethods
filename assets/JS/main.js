7767c9bb374214ea34ee6cc34717220ef199082b
Lamiiii <lamiyekazimov37@gmail.com>
2024-09-18 14:18:01 +0400

    string methods task

---
+++ assets/JS/main.js
@@ -0,0 +1,180 @@
+// 1
+// let counts="AIEOaieo";
+// let count=0
+// function myVowels(str){
+//     for(let item of str){
+//        if(counts.includes(item)){
+//         count++
+//        }
+//     }
+//     console.log(count);
+
+// }
+
+// myVowels("Kazimova Lamiye");
+
+//2
+// function strTers(str){
+
+//     const split=str.split("")
+// const reverse=split.reverse()
+// const join=reverse.join("")
+
+//     console.log(join);
+
+// }
+
+// strTers(" Kazimova Lamiye")
+
+// function strTers(str){
+//  let sum=" ";
+// for(let i=str.length-1;i>=0;i--){
+//      sum+=str[i]
+//     }
+// console.log(sum);
+
+// }
+// strTers("Lamiye");
+
+//3
+// function Str(str){
+// if(str===str.split("").reverse().join("")) {
+//     console.log(str);
+
+// }else{
+//     console.log("aid deyil");
+
+// }
+
+// }
+
+// Str("ana");
+
+// function Str(str,target){
+//     for(let i=0;i<str.length;i++){
+//         for(let j=target.length-1;j>=0;j--){
+//             if(str===target){
+//                 return str
+
+//             }
+//         }
+
+//     }
+//     return false
+// }
+
+// console.log(Str("ana","ana"));
+
+// console.log(Str("an","na"));
+
+//5
+
+// function removeDuplicates(str) {
+//     return [...new Set(str)].join("");
+// }
+
+// console.log(removeDuplicates("leyla"));
+
+
+
+//6
+// function myStr(str){
+
+//     let trimm=str.trim();
+//      let strr=trimm.split(" ");
+//      let first=strr[0]
+//      for(let i=0;i<strr.length;i++){
+//        if(first.length<strr[i].length){
+//           first=strr[i]
+          
+//        }
+//      }
+    
+     
+//      console.log(first);
+     
+     
+      
+    
+      
+      
+//     }
+     
+    
+    
+     
+//     myStr(" Lamiye uni-ye gedir  uniiiii hec yaxsi deyil.")
+    
+    
+    
+    //7
+    // let strr="Ramile"
+    
+    // function myStr(str){
+    // const strrr=str.replace("Ra","Ka")
+    // console.log(strrr);
+    // }
+    
+    // myStr(strr)
+    
+    //  const strr="Ramile"
+    
+    // function myStr( strr,str,target){
+    //     const umumi=strr.split(str).join(target)
+    //    console.log(umumi);
+    
+    // }
+    
+    // myStr(strr,"Ra","Ka");
+    
+    //8
+    
+    // function myStr(str) {
+    //   const strr = str.split(" ");
+    //   let longestword = "";
+    //   for (let item of strr) {
+    //     if (item.length > longestword.length) {
+    //       longestword = item;
+    //     }
+    //   }
+    //   console.log(longestword);
+    // }
+    
+    // myStr("Kazimova Lamiyeeee");
+    
+    
+    
+    
+    
+    
+    
+    //9
+    
+    // const str=" 5,6, lamiye"
+    
+    // function mystr(str){
+    //     const strr=str.split(",")
+    
+    //     for(let item of strr){
+    //        if(!isNaN(item)){
+    //         console.log(item);
+    //        }
+    //     }
+    // }
+    
+    // mystr(str)
+    
+
+
+
+
+
+
+
+    //tezeden
+
+
+    //1
+
+
+ 
